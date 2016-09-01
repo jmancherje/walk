44ad60a48a4ef8de3af5e52f04d338449b712e6a
Justin Mancherje <mancherje.justin@gmail.com>
2016-08-31 22:24:45 -0700

    some content on join team page with text input

--- components/joinTeam/JoinTeam.js
+++ components/joinTeam/JoinTeam.js
@@ -4,15 +4,43 @@ import React, {
 import {
   Text,
   View,
+  TextInput,
+  TouchableHighlight,
 } from 'react-native';
 
 import styles from './styles';
 
-export default function JoinTeam(props) {
-  return (
-    <View style={ styles.container }>
-      <Text>Join a Team</Text>
-      <Text>Enter a team code:</Text>
-    </View>
-  );
+export default class JoinTeam extends Component {
+  
+  state = {
+    text: "Enter Code!"
+  }
+
+  handleBlur = () => {
+    console.log('blur');
+    if (this.state.text.trim() === '') {
+      this.setState({ text: "Enter Code!" });
+    }
+  }
+
+  render() {
+    return (
+      <View style={ styles.container }>
+        <Text>Join a Team</Text>
+        <Text>Enter a team code:</Text>
+        <TextInput
+          style={ styles.inputCode }
+          value={ this.state.text }
+          onChangeText={ text => this.setState({ text }) }
+          onFocus={ () => this.setState({ text: '' }) }
+          onBlur={ this.handleBlur }
+        />
+        <TouchableHighlight onPress={ () => console.log(this.state.text) }>
+          <Text style={ [styles.submitButton, styles.blueText] }>
+            Join Team
+          </Text>
+        </TouchableHighlight>
+      </View>
+    );
+  }
 }
--- components/joinTeam/styles.js
+++ components/joinTeam/styles.js
@@ -7,6 +7,20 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
+  inputCode: {
+    height: 40,
+    borderColor: 'grey',
+    borderWidth: 1,
+  },
+  submitButton: {
+    alignSelf: 'center',
+    marginTop: 10,
+    fontWeight: 'bold',
+    fontSize: 25,
+  },
+  blueText: {
+    color: '#3e2ce2'
+  }
 });
 
 export default styles;
